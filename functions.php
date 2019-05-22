05b7f3b967104cf9f7097afae0b6caa9a871d47d
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-05-22 10:41:51 +0200

    added the footer
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 include_once get_stylesheet_directory() . '/vendor/autoload.php';
 
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/DoedeJaarsmaCommunicatie/fluffy-guacamole/',
+	__FILE__,
+	'biju'
+);
 
 add_theme_support('custom-logo');
-add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
